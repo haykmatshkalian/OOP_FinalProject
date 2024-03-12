a31343d49cbdc65be0b0f1a03cf2c824df13499a
Hayk M <108575923+haykmatshkalian@users.noreply.github.com>
2024-03-12 10:15:17 +0400

    Add files via upload

---
+++ FortuneCard.java
@@ -0,0 +1,16 @@
+public class FortuneCard {
+    public String name;
+    private int value;
+
+    public int collect( int value){
+        return 0;
+    }
+
+    public int pay( int value){
+        return 0;
+    }
+    
+    public int jail(){
+        return 0;
+    }
+}
---
+++ Property.java
@@ -0,0 +1,13 @@
+public class  Property{
+    public String name;
+    public String owner;
+    public int price;
+    public int rent;
+    public int countHouse;
+    public String propertyColor;
+
+
+    public boolean onProperty(int playerName){
+        return true;
+    }
+}
