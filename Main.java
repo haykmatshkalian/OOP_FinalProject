2fb2d25591a45d3e979ce28a694f39034b538845
Hayk M <108575923+haykmatshkalian@users.noreply.github.com>
2024-04-11 14:18:10 +0400

    Update "Timer" added
    Added (Timer libraries)
    {
    import javax.swing.*;
    import java.awt.event.*;
    }
    
    Main constructor which sets the timer

--- Main.java
+++ Main.java
@@ -1,8 +1,58 @@
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
+    private Timer timer;
+    private int secondsLeft;
+
+    public Main(int seconds) {
+        secondsLeft = seconds;
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (secondsLeft >= 0) {
+                    if(secondsLeft <= 5 ) {
+//                        secondsLeft --;
+                        System.out.println("Time left: " + secondsLeft + " seconds");
+                    }
+                    if(secondsLeft == 60 ) {
+//                        secondsLeft --;
+                        System.out.println("Time left: " + secondsLeft + " seconds");
+                    }
+                    if(secondsLeft == 30 ) {
+                        System.out.println("Time left: " + secondsLeft + " seconds");
+                    }
+                    if(secondsLeft == 10 ) {
+//                        secondsLeft --;
+                        System.out.println("Time left: " + secondsLeft + " seconds");
+                    }
+
+                    secondsLeft--;
+
+                } else {
+                    timer.stop();
+                    System.out.println("Game over!");
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+    public void startTimer() {
+        timer.start();
+    }
+
+    public void addTime(int additionalSeconds) {
+        secondsLeft += additionalSeconds;
+        System.out.println("Added " + additionalSeconds + " seconds. New time left: " + secondsLeft + " seconds");
+    }
+
 
     public static void main(String[] args) {
+        Main game = new Main(60); // 60 seconds timer
+        game.startTimer();
+
         SetGame setGame = new SetGame();
         Player player = new Player();
         Player player1 = new Player(player);
@@ -22,6 +72,7 @@ public class Main {
                 if (setGame.isSet(indices)) {
                     System.out.println("Valid set!");
                     player1.addScore();
+                    game.addTime(10);
                     System.out.println("Your score is: " + player1.getScore());
                     if (setGame.isDeckEmpty() && !setGame.hasSetOnBoard()) {
                         System.out.println("No more sets found. Game over.");
