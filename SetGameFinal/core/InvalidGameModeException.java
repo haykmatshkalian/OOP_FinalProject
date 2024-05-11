09ed378ffd0a216e7d75790ec800236447b4e627
Hayk M <108575923+haykmatshkalian@users.noreply.github.com>
2024-05-11 15:42:35 +0400

    Update InvalidGameModeException.java

--- SetGameFinal/core/InvalidGameModeException.java
+++ SetGameFinal/core/InvalidGameModeException.java
@@ -1,9 +1,21 @@
 package SetGameFinal.core;
 
+/**
+ * An exception thrown when an invalid game mode is specified.
+ */
 public class InvalidGameModeException extends Exception {
+
+    /**
+     * Constructs a new InvalidGameModeException with a default message.
+     */
     public InvalidGameModeException(){
         super("Invalid Game Mod");
     }
+    /**
+     * Constructs a new InvalidGameModeException with a specified message.
+     *
+     * @param message The detail message.
+     */
     public InvalidGameModeException(String message) {
         super(message);
     }
