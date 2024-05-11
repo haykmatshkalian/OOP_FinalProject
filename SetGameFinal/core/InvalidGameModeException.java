5ab1c852f2050320f262aaf9119910f4655e875a
Hayk M <108575923+haykmatshkalian@users.noreply.github.com>
2024-05-11 14:17:44 +0400

    Update InvalidGameModeException.java

--- SetGameFinal/core/InvalidGameModeException.java
+++ SetGameFinal/core/InvalidGameModeException.java
@@ -1,6 +1,9 @@
 package SetGameFinal.core;
 
 public class InvalidGameModeException extends Exception {
+    public InvalidGameModeException(){
+        super("Invalid Game Mod");
+    }
     public InvalidGameModeException(String message) {
         super(message);
     }
