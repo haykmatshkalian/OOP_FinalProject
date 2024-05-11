cda2cf006dc123ea9085581b1bfcef648a073076
Hayk M <108575923+haykmatshkalian@users.noreply.github.com>
2024-05-11 15:41:50 +0400

    Update Deck.java

--- SetGameFinal/core/Deck.java
+++ SetGameFinal/core/Deck.java
@@ -2,14 +2,29 @@ package SetGameFinal.core;
 
 import java.util.Random;
 
+/**
+ * The Deck class represents a deck of cards used in the Set game.
+ * It contains methods for generating a shuffled deck, checking if the deck is empty,
+ * and providing access to cards in the deck.
+ */
 public class Deck {
+
     public static final int DECK_SIZE = 81;
     private final Card[] deck;
     private int copyIndex;
+
+    /**
+     * Constructs a Deck object and initializes the deck.
+     */
     public Deck(){
         deck = generateDeck();
         copyIndex = 0;
     }
+
+    /**
+     * Generates and shuffles the deck of cards.
+     * @return Shuffled deck of cards.
+     */
     private Card[] generateDeck() {
         Card[] cards = new Card[DECK_SIZE];
         Card.Shape[] shapes = {Card.Shape.DIAMOND, Card.Shape.OVAL, Card.Shape.SQUIGGLES};
@@ -33,6 +48,10 @@ public class Deck {
         return cards;
     }
 
+    /**
+     * Shuffles the deck of cards using the Fisher-Yates algorithm.
+     * @param cards The array of cards to be shuffled.
+     */
     private void shuffleDeck(Card[] cards) {
         Random rnd = new Random();
         for (int i = 0; i < DECK_SIZE; i++) {
@@ -42,16 +61,35 @@ public class Deck {
             cards[r] = transfer;
         }
     }
+
+    /**
+     * Checks if the deck is empty.
+     * @return True if the deck is empty, false otherwise.
+     */
     public boolean isDeckEmpty() {
         return copyIndex >= deck.length;
     }
 
+    /**
+     * Gets the current index in the deck.
+     * @return Current index in the deck.
+     */
     public int getDeckIndex() {
         return copyIndex;
     }
+
+    /**
+     * Increments the current index and returns its previous value.
+     * @return Previous value of the current index.
+     */
     public int addCopyIndex() {
         return copyIndex++;
     }
+
+    /**
+     * Gets the deck of cards.
+     * @return Array of cards representing the deck.
+     */
     public Card[] getDeck() {
         return deck;
     }
